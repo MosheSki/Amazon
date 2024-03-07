7148b3f543ad134702c55922a18db8677d06609f
MosheSki <MoshLago@gmail.com>
2024-03-07 14:07:16 +0200

    small fixes

--- frontend/src/Store.jsx
+++ frontend/src/Store.jsx
@@ -1,5 +1,5 @@
 import { createContext, useReducer } from "react";
-import storeReducer from "./Reducers/StoreReducer.jsx";
+import storeReducer from "./Reducers/storeReducer";
 import PropTypes from "prop-types";
 
 export const Store = createContext();
